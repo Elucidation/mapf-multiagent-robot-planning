692de4dc842df1ed3b8e59957b2050df152d4a45
Sam <elucidation@gmail.com>
2023-04-17 14:43:45 -0700

    Fix #30 automated warehouse no collisions st_astar

--- dev/robot_allocator.py
+++ dev/robot_allocator.py
@@ -22,6 +22,7 @@ import logging
 import time
 # import random
 import multiagent_planner.pathfinding as pf
+from multiagent_planner.pathfinding import Position, Path
 from robot import Robot, RobotId, RobotStatus
 # from db_robot_task import DatabaseRobotTaskManager
 from world_db import WorldDatabaseManager, WORLD_DB_PATH
@@ -29,10 +30,10 @@ from inventory_management_system.Item import ItemId
 from inventory_management_system.TaskStatus import TaskStatus
 from inventory_management_system.Station import Task
 from inventory_management_system.database_order_manager import DatabaseOrderManager, MAIN_DB
-from warehouses.warehouse_loader import load_warehouse_yaml_xy, Position
+from warehouses.warehouse_loader import load_warehouse_yaml_xy
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("robot_allocator")
 # logger.setLevel(logging.DEBUG)
 log_handler = logging.StreamHandler()
@@ -49,33 +50,29 @@ JobId = NewType('JobId', int)
 class Job:
     "Build a job from a task, containing actual positions/paths for robot"
 
-    def __init__(self, job_id: JobId, grid, robot_home_zones: list[Position],
-                 item_load_zones: list[Position],
-                 station_zones: list[Position], task: Task, robot: Robot) -> None:
+    def __init__(self, job_id: JobId, robot_home_zone: Position, item_load_zone: Position,
+                 station_zone: Position, task: Task, robot: Robot) -> None:
+        # Job-related metadata
         self.job_id = job_id
         self.task = task
         self.robot_id = robot.id
 
-        # Get positions along the route
-        self.robot_home = robot_home_zones[robot.id]
-        self.item_zone = item_load_zones[task.item_id]
-        # Note: Hacky, off-by-one because sqlite3 db starts indexing at 1, not zero
-        self.station_zone = station_zones[task.station_id - 1]
-
+        # Stops on route
         self.robot_start_pos = robot.pos
+        self.item_zone = item_load_zone
+        self.station_zone = station_zone
+        self.robot_home = robot_home_zone
 
-        # Generate paths with astar
-        self.path_robot_to_item = pf.astar(
-            grid, self.robot_start_pos, self.item_zone)
-        self.path_item_to_station = pf.astar(
-            grid, self.item_zone, self.station_zone)
-        self.path_station_to_home = pf.astar(
-            grid, self.station_zone, self.robot_home)
+        # Paths
+        self.path_robot_to_item: Path = []
+        self.path_item_to_station: Path = []
+        self.path_station_to_home: Path = []
 
         # State tracker, ladder logic
         self.started = False
         self.item_picked = False
         self.item_dropped = False
+        self.returning_home = False
         self.robot_returned = False
         self.complete = False
 
@@ -84,7 +81,7 @@ class Job:
 
     def __repr__(self):
         state = [self.started, self.item_picked,
-                 self.item_dropped, self.robot_returned, self.complete]
+                 self.item_dropped, self.returning_home, self.robot_returned, self.complete]
         state = [int(s) for s in state]
         return (f'Job [Robot {self.robot_id},Task {self.task.task_id},Order {self.task.order_id}]'
                 f' Move {self.task.item_id} to {self.task.station_id}: Progress {state}')
@@ -113,8 +110,9 @@ class RobotAllocator:
         # Get all robots regardless of state
         # assume no robots will be added or removed for duration of this instance
         robots = self.wdb.get_robots()
-        # Reset Robot states
+        # Reset Robot states and drop any held items
         for robot in robots:
+            robot.held_item_id = None
             robot.state = RobotStatus.AVAILABLE
         self.wdb.update_robots(robots)
 
@@ -127,21 +125,25 @@ class RobotAllocator:
         self.allocations: dict[RobotId, Optional[Job]] = {
             robot.id: None for robot in robots}
 
-    def make_job(self, task: Task, robot: Robot):
+    def make_job(self, task: Task, robot: Robot) -> Optional[Job]:
+        """Try to generate and return job if pathing is possible, None otherwise"""
         if robot.state != RobotStatus.AVAILABLE:
             raise ValueError(
                 f'{robot} not available for a new job: ({repr(robot.state)} == '
                 f'{repr(RobotStatus.AVAILABLE)}) = {robot.state == RobotStatus.AVAILABLE}')
         if task.status != TaskStatus.OPEN:
             raise ValueError(f'{task} not open for a new job')
-
-        # TODO : This will move to DB auto increment
-        job_id = self.job_id_counter
-        self.job_id_counter = JobId(self.job_id_counter + 1)
+        # Get positions along the route
+        robot_home = self.robot_home_zones[robot.id]
+        item_zone = self.item_load_zones[task.item_id]
+        # Note: Hacky, off-by-one because sqlite3 db starts indexing at 1, not zero
+        station_zone = self.station_zones[task.station_id - 1]
 
         # Create job
-        job = Job(job_id, self.world_grid, self.robot_home_zones,
-                  self.item_load_zones, self.station_zones, task, robot)
+        job_id = self.job_id_counter  # TODO : This will move to DB auto increment
+        # Job success
+        self.job_id_counter = JobId(self.job_id_counter + 1)
+        job = Job(job_id, robot_home, item_zone, station_zone, task, robot)
         self.jobs[job_id] = job  # Track job
         self.allocations[robot.id] = job  # Track robot allocation
         # Set task state
@@ -150,8 +152,36 @@ class RobotAllocator:
         # Set robot state
         robot.state = RobotStatus.IN_PROGRESS
         self.wdb.update_robots([robot])
-
         return job
+
+    def get_current_dynamic_obstacles(self, max_t:int = 20) -> set[tuple[int, int, int]]:
+        """Return existing robot future paths as dynamic obstacles
+
+        Returns:
+            set[tuple[int, int, int]]: set of (row, col, t) dynamic obstacles with current t=-1
+        """        
+        # For dynamic obstacles, assume current moment is t=-1, next moment is t=0
+        robots = self.wdb.get_robots()  # Get current future paths for all other robots
+        dynamic_obstacles: set[tuple[int, int, int]
+                               ] = set()  # set{(row,col,t), ...}
+        for robot in robots:
+            # Add all positions along future path
+            for t, pos in enumerate(robot.future_path):
+                dynamic_obstacles.add((pos[0], pos[1], t))
+                dynamic_obstacles.add((pos[0], pos[1], t+1)) # Also add it at future time
+
+            path_t = len(robot.future_path)
+            if robot.future_path:
+                # Add final position a few times to give space for robot to move
+                last_pos = robot.future_path[path_t-1]
+                dynamic_obstacles.add((last_pos[0], last_pos[1], path_t))
+                dynamic_obstacles.add((last_pos[0], last_pos[1], path_t + 1))
+                    
+            else:
+                # Robot stationary, add current robot position up to limit
+                for t in range(max_t):
+                    dynamic_obstacles.add((robot.pos[0], robot.pos[1], t))
+        return dynamic_obstacles
 
     def get_robot(self, robot_id: RobotId):
         return self.wdb.get_robot(robot_id)
@@ -183,6 +213,11 @@ class RobotAllocator:
         # Finds all robots currently available
         return self.wdb.get_robots(query_state=str(RobotStatus.AVAILABLE))
 
+    def set_robot_error(self, robot_id: RobotId):
+        robot = self.wdb.get_robot(robot_id)
+        robot.state = RobotStatus.ERROR
+        self.wdb.update_robots([robot])
+
     def get_available_tasks(self, limit_rows: int = 5) -> list[Task]:
         return self.ims_db.get_tasks(TaskStatus.OPEN, limit_rows)
 
@@ -201,8 +236,10 @@ class RobotAllocator:
         robot = available_robots[0]
         task = available_tasks[0]
         job = self.make_job(task, robot)
-        logging.info(
-            f'**ASSIGNED TASK TO ROBOT found pair: {robot} : {job}**')
+        if job:
+            logging.info(f'ASSIGNED TASK TO ROBOT: {robot} : {job}')
+        else:
+            logging.debug('Could not create job this turn')
         return job
 
     def update(self):
@@ -213,7 +250,18 @@ class RobotAllocator:
         # Now check for any available robots and tasks
         robot_mgr.assign_task_to_robot()
 
+    def generate_path(self, pos_a: Position, pos_b: Position) -> Path:
+        """Generate a path from a to b avoiding existing robots"""
+        dynamic_obstacles = self.get_current_dynamic_obstacles()
+        return pf.st_astar(
+            self.world_grid, pos_a, pos_b, dynamic_obstacles, end_fast=True)
+
     def job_start(self, job: Job) -> bool:
+        # Try to generate new path for robot
+        job.path_robot_to_item = self.generate_path(job.robot_start_pos, job.item_zone)
+        if not job.path_robot_to_item:
+            return False  # Did not start job as no path existed yet
+
         logging.info(f'Starting job {job}')
         robot_mgr.wdb.set_robot_path(job.robot_id, job.path_robot_to_item)
         job.started = True
@@ -228,7 +276,9 @@ class RobotAllocator:
             return False
 
         # Add item to held items for robot
-        self.robot_pick_item(job.robot_id, job.task.item_id)
+        success, item_in_hand = self.robot_pick_item(job.robot_id, job.task.item_id)
+        if not success:
+            logging.error(f'Robot could not pick item for job {job}, already holding {item_in_hand}')
         job.item_picked = True
 
         logging.info(
@@ -236,6 +286,11 @@ class RobotAllocator:
         return True
 
     def job_go_to_station(self, job: Job) -> bool:
+        # Try to generate new path for robot
+        job.path_item_to_station = self.generate_path(job.item_zone, job.station_zone)
+        if not job.path_item_to_station:
+            return False  # Did not start job as no path existed yet
+        
         # Send robot to station
         robot_mgr.wdb.set_robot_path(
             job.robot_id, job.path_item_to_station)
@@ -255,6 +310,7 @@ class RobotAllocator:
         if not drop_success:
             logging.error(
                 f"Robot didn't have item to drop: {job.robot_id} held {item_id}")
+            self.set_robot_error(job.robot_id)
             return False
         elif item_id != job.task.item_id:
             logging.error(
@@ -270,13 +326,21 @@ class RobotAllocator:
             f'Item dropped! Sending robot back home for task {job.task}')
         logging.info(f'Task {job.task} complete')
 
-        # Send robot home
-        robot_mgr.wdb.set_robot_path(
-            job.robot_id, job.path_station_to_home)
         job.item_dropped = True
         return True
 
-    def job_go_home(self, job: Job) -> bool:
+    def job_return_home(self, job: Job) -> bool:
+        # Try to generate new path for robot
+        job.path_station_to_home = self.generate_path(job.station_zone, job.robot_home)
+        if not job.path_station_to_home:
+            return False  # Did not start job as no path existed yet
+        # Send robot home
+        robot_mgr.wdb.set_robot_path(
+            job.robot_id, job.path_station_to_home)
+        job.returning_home = True
+        return True
+
+    def job_arrive_home(self, job: Job) -> bool:
         # Check that robot is at home zone
         current_pos = job.get_current_robot_pos(robot_mgr.wdb)
         if current_pos != job.robot_home:
@@ -306,8 +370,10 @@ class RobotAllocator:
             return self.job_go_to_station(job)
         elif not job.item_dropped:
             return self.job_drop_item_at_station(job)
+        elif not job.returning_home:
+            return self.job_return_home(job)
         elif not job.robot_returned:
-            return self.job_go_home(job)
+            return self.job_arrive_home(job)
         return False
 
 
@@ -316,6 +382,7 @@ if __name__ == '__main__':
 
     # Main loop processing jobs from tasks
     DELAY_SEC = 0.2
+    logger.info('Robot Allocator running')
     while True:
         logging.debug('-------')
         robot_mgr.update()
@@ -323,13 +390,10 @@ if __name__ == '__main__':
         # Delay till next task
         logging.debug(f" waiting {DELAY_SEC} seconds")
         logging.debug('---')
-        logging.debug('- Current available tasks:')
-        for available_task in robot_mgr.get_available_tasks():
-            logging.debug(available_task)
+        logging.debug(f'- Current available tasks: {[task.task_id for task in robot_mgr.get_available_tasks()]}')
         logging.debug('- Current job allocations')
         for allocated_robot_id, allocated_job in robot_mgr.allocations.items():
             logging.debug(f'RobotId {allocated_robot_id} : {allocated_job}')
-        logging.debug('- Robots:')
-        logging.debug(robot_mgr.get_available_robots())
+        logging.debug(f'- Available Robots: {robot_mgr.get_available_robots()}')
         logging.debug('---')
         time.sleep(DELAY_SEC)
--- dev/start_warehouse.bat
+++ dev/start_warehouse.bat
@@ -1,11 +1,11 @@
 @REM .\start.bat
 
-wt -p "Windows Powershell" -d . --title "Order Processor" powershell -noExit "python -m inventory_management_system.order_processor reset" ; sleep 1; ^
-nt -p "Windows Powershell" -d . --title "World Simulator" powershell -noExit "python -m world_sim" ; sleep 1; ^
-nt -p "Windows Powershell" -d . --title "Robot Allocator" powershell -noExit "python -m robot_allocator" ; sleep 1; ^
-nt -p "Windows Powershell" -d . --title "Web Order Tracking" powershell -noExit "flask --app inventory_management_system.order_tracking_web_server --debug run" ; sleep 1; ^
-nt -p "Windows Powershell" -d . --title "Web World Visualizer" powershell -noExit "node env_visualizer" ; sleep 1; ^
-nt -p "Windows Powershell" -d . --title "Fake Order Creator" powershell -noExit "echo waiting...\;sleep 3\; python -m inventory_management_system.fake_order_sender" ; sleep 1; ^
+wt -p "Windows Powershell" -d . --title "Order Processor" powershell -noExit "python -m inventory_management_system.order_processor reset" ; ^
+nt -p "Windows Powershell" -d . --title "World Simulator" powershell -noExit "python -m world_sim" ; ^
+nt -p "Windows Powershell" -d . --title "Robot Allocator" powershell -noExit "python -m robot_allocator" ; ^
+nt -p "Windows Powershell" -d . --title "Web Order Tracking" powershell -noExit "flask --app inventory_management_system.order_tracking_web_server --debug run" ; ^
+nt -p "Windows Powershell" -d . --title "Web World Visualizer" powershell -noExit "node env_visualizer" ; ^
+nt -p "Windows Powershell" -d . --title "Fake Order Creator" powershell -noExit "echo waiting...\;sleep 3\; python -m inventory_management_system.fake_order_sender" ; ^
 ft -t 1
 
 @REM  fp -t 0 
