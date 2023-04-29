fad327ca237faf0b6b69ae74742111486634ec82
Sam <elucidation@gmail.com>
2023-04-29 16:59:12 -0700

    memoize order web server DB calls

--- dev/inventory_management_system/order_tracking_web_server.py
+++ dev/inventory_management_system/order_tracking_web_server.py
@@ -1,6 +1,7 @@
 """Flask Web server for seeing orders/stations being processed"""
 import json
 from datetime import datetime, timedelta
+import time
 from typing import Optional
 from flask import Flask, render_template
 from .database_order_manager import DatabaseOrderManager, MAIN_DB
@@ -8,6 +9,42 @@ from .Item import ItemId, get_item_names
 
 #  dev> flask.exe --app inventory_management_system.order_tracking_web_server --debug run
 # Add --host=0.0.0.0 for external
+
+
+class DataLoader:
+    """Helper class to memoize calls to DB for order/station data"""
+    def __init__(self) -> None:
+        self.latest_data = None
+        self.last_time = None
+        self.update_rate_s = 1
+
+    def get_data(self, subset):
+        """Get latest data, update from DB if time since last update > update rate"""
+        now = time.perf_counter()
+        if (self.last_time is None
+            or self.latest_data is None
+                or (now - self.last_time) > self.update_rate_s):
+            self.latest_data = self.get_data_from_db(subset)
+            self.last_time = now
+        return self.latest_data
+
+    def get_data_from_db(self, subset: Optional[int] = None):
+        dboi = DatabaseOrderManager(MAIN_DB)
+        # TODO : Get all necessary orders in one SQL query
+        if subset:
+            open_orders = dboi.get_orders(
+                limit_rows=subset, direction="ASC", status="OPEN")
+            finished_orders = dboi.get_orders(
+                limit_rows=subset, status="COMPLETE")
+        else:
+            open_orders = dboi.get_orders(status="OPEN")
+            finished_orders = dboi.get_orders(status="COMPLETE")
+        stations_and_tasks = dboi.get_stations_and_tasks()
+        return (open_orders, finished_orders, stations_and_tasks)
+
+
+data_loader = DataLoader()
+
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -75,16 +112,8 @@ def get_quick_json():
 
 
 def get_all_json(subset: Optional[int] = None):
-    dboi = DatabaseOrderManager(MAIN_DB)
-    # TODO : Get all necessary orders in one SQL query
-    if subset:
-        open_orders = dboi.get_orders(
-            limit_rows=subset, direction="ASC", status="OPEN")
-        finished_orders = dboi.get_orders(limit_rows=subset, status="COMPLETE")
-    else:
-        open_orders = dboi.get_orders(status="OPEN")
-        finished_orders = dboi.get_orders(status="COMPLETE")
-    stations_and_tasks = dboi.get_stations_and_tasks()
+    open_orders, finished_orders, stations_and_tasks = data_loader.get_data_from_db(
+        subset)
 
     open_tmp = render_template(
         "fragment_open_orders.html", orders=open_orders)
