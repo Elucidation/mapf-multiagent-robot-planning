c693a9c93ffad635b96dc193b60a8b72adf29034
Sam <elucidation@gmail.com>
2023-05-06 23:37:08 -0700

    slow down world sim

--- dev/world_sim.py
+++ dev/world_sim.py
@@ -286,7 +286,7 @@ if __name__ == '__main__':
     robots = [Robot(RobotId(i), (col, row))
               for i, (row, col) in enumerate(robot_home_zones)]
 
-    TIME_STEP_SEC = 1
+    TIME_STEP_SEC = 2
     world = World(grid, robots, TIME_STEP_SEC, item_load_zones,
                   station_zones, logger=logger)
     if 'reset' in sys.argv:
