bbf5ff9465b937576e3d7efce2423425f4782189
Sam <elucidation@gmail.com>
2023-05-12 22:30:07 -0700

    Disable log to file

--- dev/warehouse_logger.py
+++ dev/warehouse_logger.py
@@ -2,25 +2,41 @@
 import logging
 import logging.handlers
 import os
+import sys
 
+LOG_TO_FILE = False
 
 def create_warehouse_logger(name, folder='logs/curr/'):
     if not os.path.exists(folder):
         os.makedirs(folder)
-    root_logger = logging.getLogger()
-    file_path = f'{folder}{name}.log'
-    rotating_handler = logging.handlers.TimedRotatingFileHandler(
-        filename=file_path, when='H')
+
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    rotating_handler.setLevel(logging.DEBUG)
-    rotating_handler.setFormatter(formatter)
-    root_logger.addHandler(rotating_handler)
+    
+    if (LOG_TO_FILE):
+        # Root logger with time rotating file handler
+        root_logger = logging.getLogger()
+        file_path = f'{folder}{name}.log'
+        rotating_handler = logging.handlers.TimedRotatingFileHandler(
+            filename=file_path, when='H')
+        rotating_handler.setLevel(logging.DEBUG)
+        rotating_handler.setFormatter(formatter)
+        root_logger.addHandler(rotating_handler)
 
+    # Building the logger for this function
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    stream_logger = logging.StreamHandler()
-    stream_logger.setLevel(logging.INFO)
-    stream_logger.setFormatter(formatter)
-    logger.addHandler(stream_logger)
+
+    # All messages up to info to stdout, including error messages
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.INFO)
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
+
+    # Error messages to stderr
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+    stderr_handler.setFormatter(formatter)
+    logger.addHandler(stderr_handler)
+
     return logger
