a9e907e6085753fe76028e7829fdbe8c0afa4947
Sam <elucidation@gmail.com>
2023-05-13 16:35:38 -0700

    Add maintain min order count

--- dev/inventory_management_system/fake_order_sender.py
+++ dev/inventory_management_system/fake_order_sender.py
@@ -20,6 +20,8 @@ parser.add_argument('-n', '--num-orders', default=1, type=int)
 parser.add_argument('-d', '--delay', default=1.0, type=float)
 parser.add_argument('--max-items', default=4, type=int)
 parser.add_argument('--max-item-id', default=5, type=int)
+parser.add_argument('-k', '--keep-min', type=int,
+                    help='the min number of new orders before adding new ones')
 args = parser.parse_args()
 
 
@@ -54,6 +56,24 @@ def send_new_order_request():
         f'{i} - Added new order request to {order_request_queue} : {msg}')
 
 
+if args.keep_min:
+    logger.info(
+        f"Starting maintaining a minimum order count of {args.keep_min}")
+    while True:
+        orders_new_count = int(redis_con.llen('orders:new') or 0)
+        orders_requested_count = int(redis_con.llen('orders:requested') or 0)
+        count = orders_new_count + orders_requested_count
+        if count < args.keep_min:
+            logger.info(
+                f'Only {count} new/requested orders, adding until {args.keep_min}')
+            for i in range(args.keep_min - count):
+                send_new_order_request()
+        logger.info(f" waiting {args.delay:.2f} seconds")
+        time.sleep(args.delay)
+
+    exit()
+
+# Otherwise, continue with fixed number of orders
 logger.info("Starting publishing fake orders")
 random.seed(1234)  # Use a fixed seed to make this repeatable
 for i in range(args.num_orders):
