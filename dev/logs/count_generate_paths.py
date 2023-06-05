802223cb98f38fc53bc532d4b40c871eeb46e649
Sam <elucidation@gmail.com>
2023-06-05 11:44:11 -0700

    update count generate paths

--- dev/logs/count_generate_paths.py
+++ dev/logs/count_generate_paths.py
@@ -12,6 +12,10 @@ def load_robot_allocator_log_file(filename) -> list[dict]:
             parts = line.split(' - ')
             if not 'generate_path' in parts[3]:
                 continue
+            if 'Robot Allocator started' in parts[3]:
+                # Delete everything prior
+                data = []
+                continue
 
             # example message contains 'generate_path took 0.387 ms'
             _, _, duration_ms_str, _ = parts[3].split(' ')
@@ -20,23 +24,32 @@ def load_robot_allocator_log_file(filename) -> list[dict]:
 
 
 print('Loading data')
-data_euclid = load_robot_allocator_log_file(
-    'curr/TEST1_robot_allocator_EUCLIDEAN.log')
-data_true = load_robot_allocator_log_file(
-    'curr/TEST2_robot_allocator_TRUE.log')
+# data_euclid = load_robot_allocator_log_file(
+#     'curr/TEST1_robot_allocator_EUCLIDEAN.log')
+data_true_d = load_robot_allocator_log_file(
+    'curr/robot_allocator.log')
+data_true_a = load_robot_allocator_log_file(
+    'curr/robot_allocator.log.2023-05-16_11')
+data_true_b = load_robot_allocator_log_file(
+    'curr/robot_allocator.log.2023-05-16_12')
+data_true_c = load_robot_allocator_log_file(
+    'curr/robot_allocator.log.2023-05-16_13')
 
-
-plt.figure(figsize=(6, 4))
-plt.hist(data_euclid, bins=40, alpha=0.5, label='Euclidean Heuristic')
-plt.hist(data_true, bins=40, alpha=0.5, label='True Heuristic')
+print(len(data_true_a), len(data_true_b), len(data_true_c), len(data_true_d))
+data_true = np.concatenate([data_true_a, data_true_b, data_true_c, data_true_d], axis=0)
+print(len(data_true))
+plt.figure(figsize=(8, 6))
+# plt.hist(data_euclid, bins=40, alpha=0.5, label='Euclidean Heuristic')
+plt.hist(data_true, bins=80, alpha=1.0, label='True Heuristic', log=True)
 plt.xlabel('Processing Time (ms)')
 plt.ylabel('Frequency (count)')
-plt.title('Comparing Euclidean vs True Heuristic Functions')
+plt.title(f'generate_path histogram over {len(data_true)} entries')
+# plt.title('Comparing Euclidean vs True Heuristic Functions')
 
 min_ylim, max_ylim = plt.ylim()
-plt.axvline(data_euclid.mean(), color='blue', linestyle='dashed')
-plt.text(data_euclid.mean(), max_ylim*0.9,
-         f' Euclid-H Mean - {data_euclid.mean():.2f} ms', fontsize=6)
+# plt.axvline(data_euclid.mean(), color='blue', linestyle='dashed')
+# plt.text(data_euclid.mean(), max_ylim*0.9,
+#          f' Euclid-H Mean - {data_euclid.mean():.2f} ms', fontsize=6)
 plt.axvline(data_true.mean(), color='orange', linestyle='dashed')
 plt.text(data_true.mean(), max_ylim*0.8,
          f' True-H Mean - {data_true.mean():.2f} ms', fontsize=6)
