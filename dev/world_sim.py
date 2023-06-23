c15a90b51485d770715470bd9106a742aa87d254
Sam <elucidation@gmail.com>
2023-06-23 14:10:20 -0700

    add env TIME_STEP_SEC

--- dev/world_sim.py
+++ dev/world_sim.py
@@ -317,7 +317,7 @@ if __name__ == '__main__':
     robots = [Robot(RobotId(i), (col, row))
               for i, (row, col) in enumerate(robot_home_zones)]
 
-    TIME_STEP_SEC = 1
+    TIME_STEP_SEC = float(os.getenv("TIME_STEP_SEC", default="1"))
     world = World(grid, robots, TIME_STEP_SEC, redis_con, item_load_zones,
                   station_zones, logger=logger)
     if 'reset' in sys.argv:
